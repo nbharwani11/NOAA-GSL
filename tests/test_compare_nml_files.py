17ca8c6c923d58712fe327210ccd3bf782e5796d
Naureen Bharwani <naureenbharwani@Naureens-MacBook-Air.local>
2023-03-28 10:06:02 -0500

    Added test cases

---
+++ tests/test_compare_nml_files.py
@@ -0,0 +1,17 @@
+import argparse
+import os
+import pytest
+
+from src  import compare_nml_files
+
+noaa_file_base = os.path.join(os.path.dirname(__file__))
+
+
+def test_read_file(self):
+    '''Reads content of a file using file path.'''
+    input_file = os.path.join(noaa_file_base, "fixtures/example.nml")
+
+def test_write_file(self):
+    '''Writes content of a file using the inout file path and output file path provided'''
+    pass
+
