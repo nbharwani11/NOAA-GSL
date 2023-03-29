7cc01ba62ff428e7e36a117ed313d8531750d583
Naureen Bharwani <naureenbharwani@Naureens-MacBook-Air.local>
2023-03-29 11:15:07 -0500

    Added test case

--- src/compare_nml_files.py
+++ src/compare_nml_files.py
@@ -2,12 +2,17 @@ import f90nml
 import argparse
 
 def read_nml(filepath):
-    ''' Load a file given the filepath. Return a dict object.'''
+    '''Load a file given the filepath. Return a dict object.'''
     with open(filepath) as nml_file:
         nml = f90nml.read(nml_file)
+        return nml
 
-def write(input_path, output_path):
-    ''' Write an object to a file at the output_path provided.'''
+def write_nml(input_path, output_path):
+    '''Write an object to a file at the output_path provided.'''
     with open(input_path) as nml_file:
         nml = f90nml.write(nml_file)
+        return nml
 
+def compare_nml(nml_file1, nml_file2):
+    '''Takes in two namelist files. Parses each of the namelist files. Compare values of the namelist files. Will return the differences between the two files.'''
+    pass
--- tests/test_compare_nml_files.py
+++ tests/test_compare_nml_files.py
@@ -1,16 +1,34 @@
 import argparse
 import f90nml
+import os
 
 from src import compare_nml_files
 
 noaa_file_base = os.path.join(os.path.dirname(__file__))
 
 
-def test_read_file(self):
-    '''Reads content of a file using file path.'''
+def test_read_nml():
+    '''Tests reading content of a file. Confirms the output is as expected.'''
     input_file = os.path.join(noaa_file_base, "fixtures/example.nml")
+    result = read_nml.compare_nml_files(input_file)
 
-def test_write_file(self):
+    outcome=\
+    """&salad
+  fruit = 'papaya'
+  vegetable = 'eggplant'
+  how_many = '17'
+  dressing = 'ranch'
+/
+"""
+    assert result == outcome 
+
+
+def test_write_nml():
     '''Writes content of a file using the inout file path and output file path provided.'''
     pass
 
+
+def test_compare_nml():
+    '''
+    '''
+    pass
