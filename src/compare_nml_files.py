4a0a3f1381c47ce5bf06c5032c125d30ab03993a
Naureen Bharwani <naureenbharwani@Naureens-MacBook-Air.local>
2023-03-30 12:51:32 -0500

    Update to compare nml main function

--- src/compare_nml_files.py
+++ src/compare_nml_files.py
@@ -1,5 +1,6 @@
-import f90nml
+#!/usr/bin/python3
 import argparse
+import f90nml
 
 def read_nml(filepath):
     '''Load a file given the filepath. Return a dict object.'''
@@ -7,12 +8,38 @@ def read_nml(filepath):
         nml = f90nml.read(nml_file)
         return nml
 
+
 def write_nml(input_path, output_path):
     '''Write an object to a file at the output_path provided.'''
     with open(input_path) as nml_file:
         nml = f90nml.write(nml_file)
         return nml
 
+
 def compare_nml(nml_file1, nml_file2):
-    '''Takes in two namelist files. Parses each of the namelist files. Compare values of the namelist files. Will return the differences between the two files.'''
+    '''Takes in two namelist files. Parses each of the namelist files.
+     Compare values of the namelist files. Will return the differences between the two files.'''
     pass
+
+
+def main():
+    '''Takes input from user of two Fortran namelist files.
+    Returns differences between the files.'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nml_filepath1")
+    parser.add_argument("nml_filepath2")
+    args = parser.parse_args()
+
+    print(args)
+    nml_file1 = read_nml(args.nml_filepath1)
+    nml_file2 = read_nml(args.nml_filepath2)
+
+    
+    print(nml_file1)
+    print(nml_file2)
+    return args
+
+
+
+if __name__ == "__main__":
+    main()
